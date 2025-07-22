c595e755dbcef720ee2607afdec6a9283a29e1df
LinuxGamerr <gamingworld3654@gmail.com>
2025-07-22 08:46:00 +0530

    final commit

--- src/app/calendar/page.tsx
+++
@@ -1,9 +0,0 @@
-import RecurringDatePicker from "@/components/RecurringDatePicker"
-
-export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gray-100 dark:bg-gray-950 p-4">
-      <RecurringDatePicker />
-    </main>
-  )
-}
